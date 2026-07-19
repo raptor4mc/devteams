e57b48cbbf2b9c8ffb3854df41967a14cf09b1e1
Raptor4 <raptor4mc@gmail.com>
2026-07-19 21:08:47 +0200

    Update and rename frontend/index.tsx to frontend/app/page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+
+// app/page.tsx
+
+// 1. Define the component as a function
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello DevTeams</h1>
+      <p>Your social media app is running!</p>
+    </main>
+  );
+}   
